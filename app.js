af96ffb94aa264f341ceb8ef34bcbc3dfb02cd0e
dabeenkim <ekqls6812@naver.com>
2023-03-12 21:18:47 +0900

    뭔가 이상해

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const usersRouter = require("./routes/users.js");
+const postsRouter = require("./routes/posts.js");
+const commentsRouter = require("./routes/comments.js");
+
+const app = express();
+const PORT = 3016;
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use('/', usersRouter);
+app.use("/posts", [postsRouter, commentsRouter]);
+
+
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
+})
