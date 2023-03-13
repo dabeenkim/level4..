ee982f6368e77a26bc9e1dc317cbac4e21da436d
dabeenkim <ekqls6812@naver.com>
2023-03-13 14:44:54 +0900

    git고치기 2트..

--- routes/likes.js
+++ routes/likes.js
@@ -7,7 +7,7 @@ const authMiddleware = require("../middlewares/auth-middleware");
 
 //좋아요 조회api
 router.get('/', authMiddleware, async (req, res) => {
-    // try {
+    try {
       const { userId } = res.locals.user;
   
       const parseLikePostsModel = (likes) => {
@@ -56,30 +56,30 @@ router.get('/', authMiddleware, async (req, res) => {
       return res.status(200).json({
         posts: posts,
       });
-    // } catch (error) {
-    //   console.error(`${req.method} ${req.originalUrl} : ${error.message}`);
-    //   return res.status(400).json({
-    //     errorMessage: '좋아요 게시글 조회에 실패하였습니다.',
-    //   });
-    // }
+    } catch (error) {
+      console.error(`${req.method} ${req.originalUrl} : ${error.message}`);
+      return res.status(400).json({
+        errorMessage: '좋아요 게시글 조회에 실패하였습니다.',
+      });
+    }
   });
 
 
 
 //좋아요업데이트api
 router.put('/:postId', authMiddleware, async (req, res) => {
-    // try {
+    try {
       const { postId } = req.params;
       const { userId } = res.locals.user;
     //   console.log(nickname)
   
-    //   const isExistPost = await Posts.findByPk(postId);
+      const isExistPost = await Posts.findByPk(postId);
   
-    //   if (!isExistPost) {
-    //     return res.status(404).json({
-    //       errorMessage: '게시글이 존재하지 않습니다.',
-    //     });
-    //   }
+      if (!isExistPost) {
+        return res.status(404).json({
+          errorMessage: '게시글이 존재하지 않습니다.',
+        });
+      }
   
       let isLike = await Likes.findOne({
         where: {
@@ -99,17 +99,17 @@ router.put('/:postId', authMiddleware, async (req, res) => {
           where: { PostId: postId, UserId: userId },
         });
   
-        return res
+        return res  
           .status(200)
           .json({ message: '게시글의 좋아요를 취소하였습니다.' });
       }
-    // } catch (error) {
-    //   console.error(`${req.method} ${req.originalUrl} : ${error.message}`);
-    //   return res.status(400).json({
-    //     errorMessage: '게시글 좋아요에 실패하였습니다.',
-    //   });
-    // }
-  });
+    } catch (error) {
+      console.error(`${req.method} ${req.originalUrl} : ${error.message}`);
+      return res.status(400).json({
+        errorMessage: '게시글 좋아요에 실패하였습니다.',
+      });
+    }
+});
 
 
 module.exports = router;
